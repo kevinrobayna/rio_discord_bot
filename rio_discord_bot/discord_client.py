bc150dcafc9ad0213c1b49336351f3f896e8eacf
Kevin Robayna <me@kevinrobayna.com>
2021-03-09 20:43:12 +0000

    Add Discord client (#4)

---
+++ rio_discord_bot/discord_client.py
@@ -0,0 +1,34 @@
+import discord
+
+
+class DiscordClient(discord.Client):
+    def __init__(self, channel: int, players: list):
+        self.channel: int = channel
+        self.players: list = players
+
+        super().__init__()
+
+    async def on_ready(self):
+        print(f"{self.user} is connected!")
+        channel = self.get_channel(self.channel)
+        lines = [
+            "Hello! Ready to bring you RaiderIO information 🎉",
+            "I'll only list the following characters: If I'm missing any, please add them here: TBD",
+            "",
+            "\n".join(self.players),
+        ]
+        await channel.send("\n".join(lines))
+
+    async def on_member_join(self, member):
+        print(member)
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+        if str(message.channel.id) != str(self.channel):
+            return
+
+        if message.content.lower().startswith("!rio rank"):
+            channel = self.get_channel(self.channel)
+            await channel.send("TBD, but Sylphyl Rocks!!! sozz Krugdir")
+        return
